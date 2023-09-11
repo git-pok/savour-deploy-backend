4647bda17779aaef295fee8c27071043d3c1e433
Vincent Impreveduto <vincent.impreveduto@icloud.com>
2023-09-11 14:42:13 -0400

    Updated db uri.

--- config.js
+++ config.js
@@ -4,8 +4,8 @@ let BCRYPT_WORK_FACTOR = process.env.NODE_ENV === "test" ? 1 : 12;
 
 let DB_URI;
 
-DB_URI = process.env.NODE_ENV === "test"
-    ? "postgresql:///savour_test" : "postgresql:///savour";
+DB_URI = process.env.NODE_ENV === "prod"
+    ? "postgres://savour_database_user:paaQlqsqSdwojXNSyxtwT4npFwA5xnby@dpg-cjvlshh5mpss73dsavp0-a/savour_database" : "postgresql:///savour";
 
 let db = new Client ({ 
     connectionString: DB_URI
--- server.js
+++ server.js
@@ -2,6 +2,6 @@ const app = require("./app.js");
 
 app.listen(3000, () => {
     console.log(
-        "Welcome to Savour! App hosted on: localhost:3000"
+        "Welcome to Savour!"
     );
 })
